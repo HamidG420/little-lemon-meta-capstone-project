a9fb17af1fc5b4d8fe3c977a0178ffed41741e15
HamidG420 <hamidgoodarzi131@gmail.com>
2023-09-15 22:35:12 +0330

    Adding Semantic & Meta/OG Tags To Project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <div className="App">HomePage</div>;
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import logo from '../Assets/Logo.svg';
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="Little Lemon Logo" />
+    </header>
+  );
+};
+export default Header;
